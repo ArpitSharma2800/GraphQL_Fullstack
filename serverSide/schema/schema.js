1c454cce0c59d5d4a8e695cb6aec3b3ee64c367b
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-04-06 20:03:22 +0530

    author query

--- serverSide/schema/schema.js
+++ serverSide/schema/schema.js
@@ -4,7 +4,9 @@ const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLSchema,
-    GraphQLString
+    GraphQLString,
+    GraphQLInt,
+    GraphQLID
 } = require('graphql');
 
 var books = [{
@@ -24,18 +26,51 @@ var books = [{
     },
 ];
 
+var authors = [{
+        name: 'Patrick Rothfuss',
+        age: 44,
+        id: '1'
+    },
+    {
+        name: 'Brandon Sanderson',
+        age: 42,
+        id: '2'
+    },
+    {
+        name: 'Terry Pratchett',
+        age: 66,
+        id: '3'
+    }
+];
+
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {
-            type: GraphQLString
+            type: GraphQLID
         },
         name: {
             type: GraphQLString
         },
         genre: {
             type: GraphQLString
+        },
+
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
         },
 
     })
@@ -48,7 +83,7 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {
                 id: {
-                    type: GraphQLString
+                    type: GraphQLID
                 }
             },
             resolve(parent, args) {
@@ -56,7 +91,21 @@ const RootQuery = new GraphQLObjectType({
                     id: args.id
                 });
             }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+            resolve(parent, args) {
+                return _.find(authors, {
+                    id: args.id
+                });
+            }
         }
+
     }
 })
 
