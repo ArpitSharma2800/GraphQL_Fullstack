ec088138e669bfcf5cd1894db237eec0c5686887
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-04-15 21:43:09 +0530

    fixed mutation and re fetching

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -14,7 +14,7 @@ function BookList() {
       <ul id="book-list">
         {data.books.map(book => (
           <li key={book.id}>
-            {book.name}, {book.authorId}
+            {book.name}
           </li>
         ))}
       </ul>
--- client/src/components/addBook.jsx
+++ client/src/components/addBook.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import { useQuery, gql, useMutation } from '@apollo/client';
-import { getAuthorsQuery } from '../queries/queries';
+import { getAuthorsQuery, getBooksQuery } from '../queries/queries';
 
 const AUTHOR_QUERY = getAuthorsQuery;
 
 const ADD_BOOK = gql`
-  mutation addAuthor($name: String!, $genre: String!, $authorId: ID!) {
-    addAuthor( input : {name: $name, genre: $genre, authorId:$authorId}) {
-      name
-      genre
+    mutation AddBook($name: String!, $genre: String!, $authorId: ID!){
+        addBook(name: $name, genre: $genre, authorId: $authorId){
+            name
+            id
+        }
     }
-  }
 `;
 
 
@@ -18,9 +18,9 @@ const ADD_BOOK = gql`
 function AddBook() {
     const { loading, error, data } = useQuery(AUTHOR_QUERY);
     const [AddBook] = useMutation(ADD_BOOK);
-    const [name2, setName] = useState(null);
-    const [genre2, setGenre] = useState(null);
-    const [authorId2, setAuthorId] = useState(null);
+    const [name, setName] = useState(null);
+    const [genre, setGenre] = useState(null);
+    const [authorId, setAuthorId] = useState(null);
 
 
     function displayAuthors() {
@@ -40,8 +40,10 @@ function AddBook() {
         e.preventDefault()
         console.log(e);
         // use the addBookMutation // adds a book, but with no values
-        console.log(name2, genre2, authorId2);
-        AddBook({ variables: { name: name2, genre: genre2, authorId: authorId2 } });
+        console.log(name, genre, authorId);
+        AddBook({ variables: { name: name, genre: genre, authorId: authorId }, refetchQueries: [{ query: getBooksQuery }] });
+
+
     }
     return (
         <form id="add-book" onSubmit={(e) => submitForm(e)}>
